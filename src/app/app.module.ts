29c9bf92858354e2e357d978bad161ae18c43402
Maxime Tournier <mxm.tournier@gmail.com>
2024-02-29 13:38:54 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHomeComponent } from './home/page-home/page-home.component';
+import { PageActualityComponent } from './actuality/page-actuality/page-actuality.component';
+import { DetailActualityComponent } from './actuality/detail-actuality/detail-actuality.component';
+import { PageMygameComponent } from './mygame/page-mygame/page-mygame.component';
+import { PageAccountComponent } from './account/page-account/page-account.component';
+import { PageGameComponent } from './game/page-game/page-game.component';
+import { DetailGameComponent } from './game/detail-game/detail-game.component';
+import { NavbarComponent } from "./-global/navbar/navbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHomeComponent,
+    PageActualityComponent,
+    DetailActualityComponent,
+    PageMygameComponent,
+    PageAccountComponent,
+    PageGameComponent,
+    DetailGameComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
