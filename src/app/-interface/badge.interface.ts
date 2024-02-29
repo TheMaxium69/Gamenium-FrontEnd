193f9b0993fd4aca2d5eafe1dca138a703d01c36
AngeloNix64 <angelofernandez15@outlook.fr>
2024-02-29 14:01:28 +0100

    Update interface

---
+++ src/app/-interface/badge.interface.ts
@@ -0,0 +1,24 @@
+import {PictureInterface} from "/picture.interface";
+
+export interface BadgeInterface {
+
+  id:number,
+  picture_id: PictureInterface[]|undefined,
+  name:string,
+  creationDateTime: string,
+
+}
+
+
+// {
+//   "id": 1,
+//   "name": "fvdfsfvdsqd",
+//   "CreatedAt": "2024-02-29T12:30:36+00:00",
+//   "picture": {
+//       "id": 2,
+//       "url": "nuerdn",
+//       "idUser": 3,
+//       "postedAt": "2024-02-23T11:49:39+00:00",
+//       "ip": "nrteynt"
+//   }
+// },
