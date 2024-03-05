0edba0a360736f8389315e240292a0442cfa99e0
Maxime Tournier <mxm.tournier@gmail.com>
2024-03-05 16:52:24 +0100

    update

--- src/app/game/search-page/search-page.component.ts
+++ src/app/game/search-page/search-page.component.ts
@@ -1,5 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {AfterViewInit, Component, OnChanges, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {GameInterface} from "../../-interface/game.interface";
+import {GameService} from "../../-service/game.service";
+import {AppComponent} from "../../app.component";
 
 @Component({
   selector: 'app-search-page',
@@ -8,28 +11,56 @@ import {ActivatedRoute} from "@angular/router";
 })
 export class SearchPageComponent implements OnInit{
 
-  searchValue: string | null = '';
+  searchValue: string = '';
+  searchType: string | null = '';
+
+  games: GameInterface[] = [];
 
   constructor(
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute,
+    private router: Router,
+    private gameService: GameService,
+    private app: AppComponent) {
   }
 
   ngOnInit(): void {
 
-    // this.searchValue = this.route.snapshot.paramMap.get('value');// Récupérer la valeur du paramètre "value"
+    let searchValueTemp = this.route.snapshot.paramMap.get('value');
+    if (searchValueTemp){
+      this.searchValue = searchValueTemp;
+    }
+    this.searchType = this.route.snapshot.paramMap.get('type');
 
-    const valueParam = this.route.snapshot.paramMap.get('value');
-
-    // Récupérer la valeur du paramètre "type"
-    const typeParam = this.route.snapshot.paramMap.get('type');
-
-    // Faire quelque chose avec les valeurs récupérées
-    console.log('Value:', valueParam);
-    console.log('Type:', typeParam);
-
-    // console.log("Search Value", this.searchValue)
+    if (this.searchType == 'game'){
+      this.searchGame()
+    } else if (this.searchType == 'user'){
+      this.searchUser()
+    } else if (this.searchType == 'provider'){
+      this.searchProvider()
+    }
 
   }
 
+
+  changeType(type:string) {
+    this.searchType = type;
+    this.router.navigate(['/search/'+ type +'/' + this.searchValue]);
+  }
+
+  searchGame(): void {
+
+    this.gameService.searchGames(this.searchValue, 100, this.app.setURL()).subscribe((results) => {
+      this.games = results;
+    });
+
+  }
+
+  searchUser(): void {
+    console.log("Faire le search User");
+  }
+
+  searchProvider(): void {
+    console.log("Faire le search Provider");
+  }
 
 }
