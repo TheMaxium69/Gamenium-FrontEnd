f00dee09b82e866000fb26d9d28af882946974ec
Threonaum <lepolak01@gmail.com>
2024-03-12 09:43:11 +0100

    update

--- src/app/actuality/provider/provider.component.ts
+++ src/app/actuality/provider/provider.component.ts
@@ -20,6 +20,7 @@ export class ProviderComponent implements OnInit{
   providerNbFollower1: number|undefined=10;
   providerNbActu: number|undefined=8;
   providerNbGame: number|undefined=2689;
+  providerBackgroundColor: string|undefined='green';
   //end
 
   constructor(private route: ActivatedRoute,
