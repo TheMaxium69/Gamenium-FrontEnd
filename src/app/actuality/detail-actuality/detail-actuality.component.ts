8bd763cf4bfded12af5ea7caff2f067d18acab7b
Oscar <oscar.boguszewski@gmail.com>
2024-03-14 14:19:19 +0100

    update

--- src/app/actuality/detail-actuality/detail-actuality.component.ts
+++ src/app/actuality/detail-actuality/detail-actuality.component.ts
@@ -15,6 +15,15 @@ export class DetailActualityComponent implements OnInit{
   actualitySelected: PostActuInterface|undefined;
   noneActu: boolean = false;
 
+// fake variables
+  actuTitle: string|undefined = 'Titre';
+  actuContent: string|undefined = 'Contenu';
+  nbLike: number|undefined = 150;
+  nbCommentaire: number|undefined = 20;
+
+//End fake variables
+
+
   constructor(private route: ActivatedRoute,
               private postActu: PostActuService,
               private app: AppComponent) {}
@@ -31,11 +40,11 @@ export class DetailActualityComponent implements OnInit{
   getActuById(id:number){
 
     this.postActu.getPostActuById(id, this.app.setURL()).subscribe((reponsePostActu) => {
-
+      console.log(reponsePostActu)
       if (reponsePostActu.message == "good"){
 
         this.actualitySelected = reponsePostActu.result
-
+        console.log(this.actualitySelected)
       } else {
 
         this.noneActu = true;
