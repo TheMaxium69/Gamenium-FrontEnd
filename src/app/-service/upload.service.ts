4e26018e70ac6a80d82a89ef04fe46f917dddfa8
Maxime Tournier <mxm.tournier@gmail.com>
2024-03-15 13:14:28 +0100

    Update upload.service.ts

--- src/app/-service/upload.service.ts
+++ src/app/-service/upload.service.ts
@@ -11,10 +11,29 @@ export class UploadProfilePictureService {
   constructor(private http: HttpClient) { }
 
   uploadUserPhoto(photo: File, url:string, option: {headers: HttpHeaders}):Observable<ApicallInterface> {
+
+    console.log(photo);
+
     const formData = new FormData();
     formData.append('photo', photo);
 
-    return this.http.post<ApicallInterface>(url + '/upload/pp/', formData, option);
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'application/x-www-form-urlencoded'
+    // });
+    //
+    // const headers = new HttpHeaders({
+    //   'Content-Type': 'multipart/form-data; charset=utf-8; boundary=' + Math.random().toString().substr(2),
+    //   'Accept': '*/*',
+    //   'Accept-Encoding': 'gzip, deflate, br',
+    //   'Connection': 'keep-alive',
+    //   'User-Agent': 'PostmanRuntime/7.37.0',
+    // });
+
+
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'multipart/form-data');
+
+    return this.http.post<ApicallInterface>(url + '/upload/pp/', formData, { headers: headers });
   }
 
 
