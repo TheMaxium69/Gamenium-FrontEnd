1ec792905b48a6a17c6cde76565d88c74efe69cb
Oscar <oscar.boguszewski@gmail.com>
2024-03-19 09:19:04 +0100

    Update provider

--- src/app/actuality/provider/provider.component.ts
+++ src/app/actuality/provider/provider.component.ts
@@ -1,3 +1,49 @@
+// import {Component, OnInit} from '@angular/core';
+// import {ActivatedRoute} from "@angular/router";
+// import {ProviderInterface} from "../../-interface/provider.interface";
+// import {ProviderService} from "../../-service/provider.service";
+// import {AppComponent} from "../../app.component";
+
+// @Component({
+//   selector: 'app-provider',
+//   templateUrl: './provider.component.html',
+//   styleUrls: ['./provider.component.css']
+// })
+// export class ProviderComponent implements OnInit{
+
+//   providerId: number|any;
+//   providerSelected: ProviderInterface|undefined;
+//   noneProvider:boolean|undefined = false;
+
+//   constructor(private route: ActivatedRoute,
+//               private providerService: ProviderService,
+//               private app: AppComponent) {
+//   }
+
+//   ngOnInit(): void {
+
+//     this.providerId = this.route.snapshot.paramMap.get('id');
+
+//     console.log("Provider Id", this.providerId)
+
+//     this.getProviders(this.providerId);
+
+//   }
+
+//   getProviders(id:number): void {
+//     this.providerService.getProviderById(id, this.app.setURL()).subscribe((reponseProviders) => {
+
+//       if (reponseProviders.message !== "Provider not found"){
+//         this.providerSelected = reponseProviders.result;
+//       } else {
+//         this.noneProvider = true;
+//       }
+
+//     });
+//   }
+
+// }
+
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ProviderInterface} from "../../-interface/provider.interface";
@@ -14,6 +60,15 @@ export class ProviderComponent implements OnInit{
   providerId: number|any;
   providerSelected: ProviderInterface|undefined;
   noneProvider:boolean|undefined = false;
+
+  //fake variables
+  providerNbFollower: number|undefined=10000;
+  providerNbFollower1: number|undefined=10;
+  providerNbActu: number|undefined=8;
+  providerNbGame: number|undefined=2689;
+  providerBackgroundColor: string|undefined='#107c0f';
+  actuTitle: string|undefined='test'
+  //end
 
   constructor(private route: ActivatedRoute,
               private providerService: ProviderService,
