7cca4062cc7284497f80b819250121438db5f920
AngeloNix64 <angelofernandez15@outlook.fr>
2024-03-28 09:23:19 +0100

    Update Like service aled

--- src/app/-components/like.interface/like.interface.component.ts
+++ src/app/-components/like.interface/like.interface.component.ts
@@ -7,18 +7,18 @@ import { LikeInterface } from '../../-interface/like.interface';
   templateUrl: './like.interface.component.html',
   styleUrls: ['./like.interface.component.css'],
 })
-export class LikeInterfaceComponent implements OnInit {
+export class LikeInterfaceComponent {
   Likes: LikeInterface[] = [];
 
   constructor(private LikeService: LikeService) {}
 
-  ngOnInit(): void {
-    this.getAllLikes();
-  }
+  // ngOnInit(): void {
+  //   this.getAllLikes();
+  // }
 
-  getAllLikes(): void {
-    this.LikeService.getAllLikes().subscribe((Likes) => {
-      this.Likes = Likes;
-    });
-  }
+  // getAllLikes(): void {
+  //   this.LikeService.getAllLikes().subscribe((Likes) => {
+  //     this.Likes = Likes;
+  //   });
+  // }
 }
--- src/app/-service/like.service.ts
+++ src/app/-service/like.service.ts
@@ -2,20 +2,40 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { LikeInterface } from '../-interface/like.interface';
+import {ApicallInterface} from "../-interface/apicall.interface";
 
 @Injectable({
   providedIn: 'root',
 })
 export class LikeService {
-  private apiUrl = 'http://localhost:8000/'; // A REMPLACER AVEC L'URL DE L'API
-  constructor(private http: HttpClient) {}
 
-  getLike(id: number): Observable<LikeInterface> {
-    return this.http.get<LikeInterface>(`${this.apiUrl}/like/${id}`);
+  constructor(private http: HttpClient) {}
+  createPostActuLike(url: string, postId: number, userId: number, ip: string): Observable<ApicallInterface> {
+    const body = { user_id: userId, ip, post_id: postId };
+    return this.http.post<ApicallInterface>(url + '/like', body);
   }
 
-  getAllLikes(): Observable<LikeInterface[]> {
-    return this.http.get<LikeInterface[]>(`${this.apiUrl}/likes`);
+  createCommentLike(url: string, idComment: number, userId: number, ip: string): Observable<ApicallInterface> {
+    const body = { user_id: userId, ip, comment_id: idComment };
+    return this.http.post<ApicallInterface>(url + '/like', body);
+  }
+
+  deletePostActuLike(url: string, postId: number, userId: number): Observable<ApicallInterface> {
+    const options = { body: { user_id: userId } };
+    return this.http.delete<ApicallInterface>(url + '/like/post-actu/${postId}', options);
+  }
+
+  deleteCommentLike(url: string, idComment: number, userId: number): Observable<ApicallInterface> {
+    const options = { body: { user_id: userId } };
+    return this.http.delete<ApicallInterface>(url + '/like/comment/' + idComment, options);
+  }
+
+  getPostActuLikes(url: string, idPost: number): Observable<LikeInterface[]> {
+    return this.http.get<LikeInterface[]>(url + '/like/post-actu/' + idPost);
+  }
+
+  getCommentLikes(url: string, idComment: number): Observable<LikeInterface[]> {
+    return this.http.get<LikeInterface[]>(url + '/like/comment/' + idComment);
   }
 
 }
