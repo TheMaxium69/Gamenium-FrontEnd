49bf95e224db3e1e6c24e7b3e9c1911c9ca92021
Martav69 <martav69200@gmail.com>
2024-11-12 13:00:44 +0100

    added functionnality to fetch actuality buy number

--- src/app/actuality/card-actuality/card-actuality.component.ts
+++ src/app/actuality/card-actuality/card-actuality.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {UserInterface} from "../../-interface/user.interface";
-import {AppComponent} from "../../app.component";
-import {PostActuService} from "../../-service/post-actu.service";
-import {PostActuInterface} from "../../-interface/post-actu.interface";
+import { Component, OnInit, Input } from '@angular/core';
+import { UserInterface } from "../../-interface/user.interface";
+import { AppComponent } from "../../app.component";
+import { PostActuService } from "../../-service/post-actu.service";
+import { PostActuInterface } from "../../-interface/post-actu.interface";
 import { IpService } from 'src/app/-service/ip.service';
 import { LikeService } from 'src/app/-service/like.service';
 import { LikeInterface } from 'src/app/-interface/like.interface';
@@ -14,22 +14,23 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./card-actuality.component.css']
 })
 export class CardActualityComponent implements OnInit {
+  @Input() numberOfPosts: number | undefined;
 
-  isLogIn:boolean|undefined;
-  userConnected:UserInterface|undefined;
-  userConnectedId:number|undefined;
-  postActuFollowOrAll:PostActuInterface[] = [];
-  postByProvider:PostActuInterface[] = [];
-  LikeAll: LikeInterface[]|undefined;
+  isLogIn: boolean | undefined;
+  userConnected: UserInterface | undefined;
+  userConnectedId: number | undefined;
+  postActuFollowOrAll: PostActuInterface[] = [];
+  postByProvider: PostActuInterface[] = [];
+  LikeAll: LikeInterface[] | undefined;
   providerId: number | undefined;
 
   likedStatus: { [key: number]: boolean } = {};
 
   constructor(
-    private app:AppComponent,
+    private app: AppComponent,
     private ipService: IpService,
     private likeService: LikeService,
-    private postActuService:PostActuService,
+    private postActuService: PostActuService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
@@ -42,176 +43,141 @@ export class CardActualityComponent implements OnInit {
         this.providerId = Number(params.get('id'))
       })
 
-      if (typeof this.providerId == 'number') {
-        return this.getPostByProvider(this.providerId)
+      if (typeof this.providerId === 'number') {
+        this.getPostByProvider(this.providerId);
+        return; 
       }
     }
 
-    if (this.isLogIn){
-      // Faire une recherche sur ces follow
+    if (this.isLogIn) {
+        // Faire une recherche sur ces follow
       this.userConnectedId = this.app.userConnected.id;
       this.userConnected = this.app.userConnected;
 
-      if (this.userConnected?.id){
+      if (this.userConnected?.id) {
         this.followActuByUser(this.userConnected.id);
       }
-
-      if (this.router.url.includes('provider')) {
-        this.route.paramMap.subscribe(params => {
-          this.providerId = Number(params.get('id'))
-        })
-
-        if (typeof this.providerId == 'number') {
-          return this.getPostByProvider(this.providerId)
-        }
-      }
-    
-    } else if (this.postActuFollowOrAll.length == 0) {
-      this.getActuAll()
+    } else if (this.postActuFollowOrAll.length === 0) {
+      this.getActuAll();
     }
-
   }
 
-  followActuByUser(id: number){
-
-    console.log("recupere les follows")
+  followActuByUser(id: number) {
+    console.log('Fetching followed actualities');
 
     if (!this.router.url.includes('provider')) {
       this.getActuAll();
     }
-    
   }
-  
-  getActuAll(){
-    console.log('get actu all')
-    this.postActuService.getActuAll(this.app.setURL()).subscribe((responseActu) => {
-      console.log(responseActu);
-      
-      if (responseActu.message == "good"){
+
+  getActuAll() {
+    console.log('Fetching all actualities');
+    this.postActuService.getActuAll(this.app.setURL()).subscribe(responseActu => {
+      if (responseActu.message === 'good') {
         this.postActuFollowOrAll = responseActu.result;
-        console.log(this.postActuFollowOrAll)
+
+        
         this.postActuFollowOrAll.forEach((actu: any) => {
           this.getLikeByActu(actu.id);
         });
       }
-
     });
-
   }
 
   getPostByProvider(id: number) {
-    console.log('get post by provider')
-    this.postActuService.getPostByProvider(id, this.app.setURL()).subscribe((response) => {
-      console.log(response);
-
-      if (response.message === "good") {
+    console.log('Fetching actualities by provider');
+    this.postActuService.getPostByProvider(id, this.app.setURL()).subscribe(response => {
+      if (response.message === 'good') {
         this.postActuFollowOrAll = response.result;
-        console.log(this.postByProvider);
-        this.postActuFollowOrAll.forEach((actu: any )=> {
-          this.getLikeByActu(actu.id)
+
+        
+        this.postActuFollowOrAll.forEach((actu: any) => {
+          this.getLikeByActu(actu.id);
         });
       }
-
-    })
+    });
   }
 
   getLikeByActu(idActu: number) {
-    this.likeService.getPostActuLikes(idActu, this.app.setURL()).subscribe(reponseLikeByPostActu => {
-      if (reponseLikeByPostActu.message === "good") {
+    this.likeService.getPostActuLikes(idActu, this.app.setURL()).subscribe(responseLikeByPostActu => {
+      if (responseLikeByPostActu.message === 'good') {
         let isLiked = false;
-  
-        this.LikeAll = reponseLikeByPostActu.result;
+
+        this.LikeAll = responseLikeByPostActu.result;
 
         this.LikeAll?.forEach((like: LikeInterface) => {
           if (like.user.id === this.userConnectedId) {
             isLiked = true;
           }
         });
-  
+
         this.likedStatus[idActu] = isLiked;
       }
     });
   }
-  
-  onClickLike(id: number){
+
+  onClickLike(id: number) {
     const cardActuIcon = document.querySelector(`#likeIcon${id}`);
 
-    this.ipService.getMyIp(this.app.urlIp).subscribe(reponseTyroIp => {
+    this.ipService.getMyIp(this.app.urlIp).subscribe(
+      responseTyroIp => {
+        let bodyNoJson: any = {
+          id_postactu: id,
+          ip: responseTyroIp.ip,
+          del: true
+        };
 
+        let bodyJson = JSON.stringify(bodyNoJson);
 
-      let bodyNoJson: any = {
-        "id_postactu": id,
-        "ip": reponseTyroIp.ip,
-        "del": true
-      }
-
-      let bodyJson = JSON.stringify(bodyNoJson);
-
-      this.likeService.addLikePostActu(bodyJson, this.app.setURL(), this.app.createCorsToken()).subscribe(reponseAddLikeByPostActu => {
-
-        console.log(reponseAddLikeByPostActu);
-
-        if (reponseAddLikeByPostActu.message == "good"){
-
-          if (cardActuIcon) {
-
-            if (reponseAddLikeByPostActu.result == "like is delete"){
-                this.liked(id, "del");
-                
-              } else {
-                this.liked(id, "add");
+        this.likeService
+          .addLikePostActu(bodyJson, this.app.setURL(), this.app.createCorsToken())
+          .subscribe(responseAddLikeByPostActu => {
+            if (responseAddLikeByPostActu.message === 'good') {
+              if (cardActuIcon) {
+                if (responseAddLikeByPostActu.result === 'like is delete') {
+                  this.liked(id, 'del');
+                  this.likedStatus[id] = false;
+                } else {
+                  this.liked(id, 'add');
+                  this.likedStatus[id] = true;
+                }
               }
+            }
+          });
+      },
+      error => {
+        let bodyNoJson: any = {
+          id_postactu: id,
+          del: true
+        };
 
-          }
+        let bodyJson = JSON.stringify(bodyNoJson);
 
-        }
-
-      });
-
-    },
-    (error) => {
-
-      let bodyNoJson: any = {
-        "id_postactu": id,
-        "del": true
+        this.likeService
+          .addLikePostActu(bodyJson, this.app.setURL(), this.app.createCorsToken())
+          .subscribe(responseAddLikeByPostActu => {
+            if (responseAddLikeByPostActu.message === 'good') {
+      
+            }
+          });
       }
-
-      let bodyJson = JSON.stringify(bodyNoJson);
-
-      this.likeService.addLikePostActu(bodyJson, this.app.setURL(), this.app.createCorsToken()).subscribe(reponseAddLikeByPostActu => {
-
-        console.log(reponseAddLikeByPostActu);
-
-        if (reponseAddLikeByPostActu.message == "good"){
-
-
-        }
-
-      });
-
-    });
-
+    );
   }
 
-  liked(id: number, state: String) {
+  liked(id: number, state: string) {
     const cardActuIcon = document.querySelector(`#likeIcon${id}`) as HTMLElement;
     const actu = this.postActuFollowOrAll.find(actu => actu.id === id);
 
-    if (state == "add" && cardActuIcon){
+    if (state === 'add' && cardActuIcon) {
       if (actu) {
         cardActuIcon.style.color = actu.Provider?.color ? actu.Provider?.color : 'red';
       }
-      cardActuIcon.classList.remove("ri-heart-line");
-      cardActuIcon.classList.add("ri-heart-fill");
-
-    } else if (state == "del" && cardActuIcon) {
+      cardActuIcon.classList.remove('ri-heart-line');
+      cardActuIcon.classList.add('ri-heart-fill');
+    } else if (state === 'del' && cardActuIcon) {
       cardActuIcon.style.color = '#2e3a59';
-      cardActuIcon.classList.add("ri-heart-line");
-      cardActuIcon.classList.remove("ri-heart-fill");
-
+      cardActuIcon.classList.add('ri-heart-line');
+      cardActuIcon.classList.remove('ri-heart-fill');
     }
   }
-
-
-
 }
