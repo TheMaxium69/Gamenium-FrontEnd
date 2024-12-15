0fde893f49ee1591ef0e1f6c3784d709c0c3c49b
TheMaxium69 <mxm.tournier@gmail.com>
2024-12-15 01:43:37 +0100

    final color

--- src/app/mygame/no-account-profile/no-account-profile.component.ts
+++ src/app/mygame/no-account-profile/no-account-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AppComponent} from "../../app.component";
 
 @Component({
   selector: 'app-no-account-profile',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./no-account-profile.component.css']
 })
 export class NoAccountProfileComponent {
+
+  constructor(protected app:AppComponent) { }
+
 
 }
