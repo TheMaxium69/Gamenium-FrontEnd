a6aceb9316c5a934fee30bde29414e7228aa3553
TheMaxium69 <mxm.tournier@gmail.com>
2025-02-09 01:40:43 +0100

    hmp view

--- src/app/-global/view-myplatform/view-myplatform.component.ts
+++ src/app/-global/view-myplatform/view-myplatform.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {AppComponent} from "../../app.component";
 
 @Component({
-  selector: 'app-view-myplatform',
+  selector: 'modal-view-myplatform',
   templateUrl: './view-myplatform.component.html',
   styleUrls: ['./view-myplatform.component.css']
 })
 export class ViewMyplatformComponent {
+
+
+  @Input()
+  isColor: string = this.app.colorDefault;
+
+  constructor(protected app:AppComponent) { }
+
+  closeModal(){
+
+    const body = document.body;
+    const shadow = document.getElementById('shadow');
+
+    if (shadow) {
+
+
+      shadow.remove(); // Remove the shadow div element from the DOM
+
+      body.style.overflow = ''; // Re-enable scrolling by clearing the overflow style
+      body.style.paddingRight = ''; // Clear paddingRight style
+      body.classList.remove('modal-open'); // Remove 'modal-open' class
+
+      const viewGameDiv = document.getElementById('viewGame');
+      if (viewGameDiv) {
+        viewGameDiv.classList.remove('show');
+      }
+
+    }
+
+  }
 
 }
