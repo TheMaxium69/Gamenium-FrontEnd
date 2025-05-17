680671630e1bc46bba4e59ee2776c139c1b54701
TheMaxium69 <mxm.tournier@gmail.com>
2025-05-17 18:17:36 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -68,7 +68,7 @@ export class AppComponent {
 
 
   //%     API - GAMENIUM      %//
-    AppEnv: string = "PROD"; // DEV or PROD or PRODMAX
+    AppEnv: string = "PRODMAX"; // DEV or DEVMAX or PROD or PRODMAX
     urlApiDev: string = "http://127.0.0.1:8000";
     urlApiDevMax: string = "https://127.0.0.1:8000";
     urlApiProd: string = "http://vps216.tyrolium.fr:8000";
